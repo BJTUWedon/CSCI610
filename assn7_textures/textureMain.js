937b16f8aa29cc9d9c95a5df640daa88eb856daf
Wedon <604543216@qq.com>
2022-04-25 17:34:03 +0800

    Add files via upload

--- assn7_textures/textureMain.js
+++ assn7_textures/textureMain.js
@@ -9,7 +9,7 @@
 
   // the textures
   let worldTexture;
-  
+
   // VAOs for the objects
   var mySphere = null;
   var myCube = null;
@@ -34,22 +34,30 @@
 // Any additional images that you include will need to
 // set up as well.
 //
+let fun = () => console.log('time out');
+let sleep = function(fun,time){
+  setTimeout(()=>{
+    fun();
+  },time);
+}
+
+
 function setUpTextures(){
-    
+
     // get some texture space from the gpu
     worldTexture = gl.createTexture();
     gl.bindTexture(gl.TEXTURE_2D, worldTexture);
-    
+
     // load the actual image
     var worldImage = document.getElementById ('world-texture')
     worldImage.crossOrigin = "";
-        
+
     // bind the texture so we can perform operations on it
     gl.bindTexture (gl.TEXTURE_2D, worldTexture);
-        
+    sleep(fun,1);
     // load the texture data
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, worldImage.width, worldImage.height, 0, gl.RGBA, gl.UNSIGNED_BYTE, worldImage);
-        
+
     // set texturing parameters
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
@@ -61,46 +69,46 @@ function setUpTextures(){
 // current texture
 //
 function drawCurrentShape () {
-    
+
     // which shape are we drawing
     var object = mySphere;
     if (nowShowing == "Cube") object = myCube;
-    
+
     // may need to set different parameters based on the texture
     // you are using...The current texture is found in the global variable
     // curTexture.   If will have the value of "globe", "myimage" or "proc"
-    
+
     // which program are we using
     var program = sphereGlobeProgram;
-    
+
     // set up your uniform variables for drawing
     gl.useProgram (program);
-    
+
     // set up texture uniform & other uniforms that you might
     // have added to the shader
     gl.activeTexture (gl.TEXTURE0);
     gl.bindTexture (gl.TEXTURE_2D, worldTexture);
     gl.uniform1i (program.uTheTexture, 0);
-    
+
     // set up rotation uniform
     gl.uniform3fv (program.uTheta, new Float32Array(angles));
 
     //Bind the VAO and draw
     gl.bindVertexArray(object.VAO);
     gl.drawElements(gl.TRIANGLES, object.indices.length, gl.UNSIGNED_SHORT, 0);
-    
+
 }
 
 // Create a program with the appropriate vertex and fragment shaders
 function initProgram (vertexid, fragmentid) {
-    
+
   // set up the per-vertex program
   const vertexShader = getShader(vertexid);
   const fragmentShader = getShader(fragmentid);
 
   // Create a program
   let program = gl.createProgram();
-  
+
   // Attach the shaders to this program
   gl.attachShader(program, vertexShader);
   gl.attachShader(program, fragmentShader);
@@ -116,12 +124,12 @@ function initProgram (vertexid, fragmentid) {
   // for easy access later in the code
   program.aVertexPosition = gl.getAttribLocation(program, 'aVertexPosition');
   program.aUV = gl.getAttribLocation(program, 'aUV');
-    
+
   // uniforms - you will need to add references for any additional
   // uniforms that you add to your shaders
   program.uTheTexture = gl.getUniformLocation (program, 'theTexture');
   program.uTheta = gl.getUniformLocation (program, 'theta');
-    
+
   return program;
 }
 
@@ -134,15 +142,15 @@ function initProgram (vertexid, fragmentid) {
 // general call to make and bind a new object based on current
 // settings..Basically a call to shape specfic calls in cgIshape.js
 function createShapes() {
-    
+
     // the sphere
     mySphere = new Sphere (20,20);
     mySphere.VAO = bindVAO (mySphere, sphereGlobeProgram);
-    
+
     // the cube
     myCube = new Cube (20);
     myCube.VAO = bindVAO (myCube, sphereGlobeProgram);
-    
+
 }
 
 
@@ -196,11 +204,11 @@ function createShapes() {
 // need to be set
 //
 function bindVAO (shape, program) {
-    
+
     //create and bind VAO
     let theVAO = gl.createVertexArray();
     gl.bindVertexArray(theVAO);
-    
+
     // create, bind, and fill buffer for vertex locations
     // vertex locations can be obtained from the points member of the
     // shape object.  3 floating point values (x,y,z) per vertex are
@@ -210,7 +218,7 @@ function bindVAO (shape, program) {
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(shape.points), gl.STATIC_DRAW);
     gl.enableVertexAttribArray(program.aVertexPosition);
     gl.vertexAttribPointer(program.aVertexPosition, 3, gl.FLOAT, false, 0, 0);
-    
+
     // create, bind, and fill buffer for uv's
     // uvs can be obtained from the uv member of the
     // shape object.  2 floating point values (u,v) per vertex are
@@ -220,7 +228,7 @@ function bindVAO (shape, program) {
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(shape.uv), gl.STATIC_DRAW);
     gl.enableVertexAttribArray(program.aUV);
     gl.vertexAttribPointer(program.aUV, 2, gl.FLOAT, false, 0, 0);
-    
+
     // Setting up element array
     // element indicies can be obtained from the indicies member of the
     // shape object.  3 values per triangle are stored in this
@@ -233,19 +241,19 @@ function bindVAO (shape, program) {
     gl.bindVertexArray(null);
     gl.bindBuffer(gl.ARRAY_BUFFER, null);
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
-    
+
     return theVAO;
 }
 
 
 
-  
+
   // We call draw to render to our canvas
   function draw() {
     // Clear the scene
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
     gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
- 
+
     // draw your shapes
     drawCurrentShape ();
 
@@ -257,7 +265,7 @@ function bindVAO (shape, program) {
 
   // Entry point to our application
   function init() {
-      
+
     // Retrieve the canvas
     const canvas = document.getElementById('webgl-canvas');
     if (!canvas) {
@@ -274,33 +282,33 @@ function bindVAO (shape, program) {
         console.error(`There is no WebGL 2.0 context`);
         return null;
       }
-      
+
     // Set the clear color to be black
     gl.clearColor(0, 0, 0, 1);
-      
+
     // some GL initialization
     gl.enable(gl.DEPTH_TEST);
     gl.enable(gl.CULL_FACE);
-    
+
     gl.cullFace(gl.BACK);
     gl.frontFace(gl.CCW);
     gl.clearColor(0.0,0.0,0.0,1.0)
     gl.depthFunc(gl.LEQUAL)
     gl.clearDepth(1.0)
     gl.pixelStorei (gl.UNPACK_FLIP_Y_WEBGL, true);
-      
+
     // deal with keypress
     window.addEventListener('keydown', gotKey ,false);
 
     // Read, compile, and link your shaders
     sphereGlobeProgram = initProgram('sphereMap-V', 'sphereMap-F');
-    
+
     // create and bind your current object
     createShapes();
-    
+
     // set up your textures
     setUpTextures();
-    
+
     // do a draw
     draw();
   }
